9ea308da7ca502414f1d24d02d555370ee9d34c2
stosiki <mikemeyer66@gmail.com>
2015-07-04 06:34:29 +0300

    added constants

--- app/src/main/java/com/example/mike/mvc3/ControllerProtocol.java
+++ app/src/main/java/com/example/mike/mvc3/ControllerProtocol.java
@@ -2,11 +2,15 @@ package com.example.mike.mvc3;
 
 /**
  * Created by mike on 6/30/2015.
+ * 04/07 added real constants
  */
 public class ControllerProtocol {
-    public static final int VALUE_ONE_UPDATE_REQUESTED = 0x01;
-    public static final int VALUE_ONE_CHANGED = 0x02;
-    public static final int UPDATE_STARTED = 0x03;
-    public static final int UPDATE_ENDED = 0x04;
-    public static final int DATA_UPDATE_ERROR = 0x05;
+    public static final int VALUE_ONE_UPDATE_REQUESTED = 1;
+    public static final int UPDATE_STARTED = 3;
+    public static final int UPDATE_ENDED = 4;
+    public static final int DATA_UPDATE_ERROR = 5;
+    public static final int ADD_EVENT_LINE = 6;
+    public static final int REMOVE_EVENT_LINE = 7;
+    public static final int ADD_EVENT = 8;
+    public static final int UNDO_ADD_EVENT = 9;
 }
